4683064dcf5bbb08d5b695086159bee356ff5cf5
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-07-16 22:13:15 +0100

    Update UserModal.tsx

--- alx-project-0x01/components/common/UserModal.tsx
+++ alx-project-0x01/components/common/UserModal.tsx
@@ -21,3 +21,34 @@
     "bs": "harness real-time e-markets"
   }
 }
+
+export interface Address {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+}
+
+export interface Company {
+  name: string;
+  catchPhrase: string;
+  bs: string;
+}
+
+export interface UserProps {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: Address;
+  phone: string;
+  website: string;
+  company: Company;
+}
+
+export interface UserModalProps {
+  user: UserProps;
+  isOpen: boolean;
+  onClose: () => void;
+  onSubmit: (post: UserProps) => void;
+}
