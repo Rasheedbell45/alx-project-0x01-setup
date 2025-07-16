9a366f1993078c689b303084fc4c18a30582a187
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-07-16 22:09:20 +0100

    Create UserModal.tsx

---
+++ alx-project-0x01/components/common/UserModal.tsx
@@ -0,0 +1,23 @@
+{
+  "id": 1,
+  "name": "Bello Rasheed",
+  "username": "Rasheedbell45",
+  "email": "rasheed@example.com",
+  "address": {
+    "street": "Ojo Road Ajegunle",
+    "suite": "Apt. 255",
+    "city": "Lagos",
+    "zipcode": "102201",
+    "geo": {
+      "lat": "-37.3159",
+      "lng": "81.1496"
+    }
+  },
+  "phone": "2348069338674",
+  "website": "hildegard.org",
+  "company": {
+    "name": "Romaguera-Crona",
+    "catchPhrase": "Multi-layered client-server neural-net",
+    "bs": "harness real-time e-markets"
+  }
+}
